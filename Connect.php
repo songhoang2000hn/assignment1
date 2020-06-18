cb3fdf1ada69903ff37dc87f86bbf48e891c9a3b
songhoang2000hn <66767531+songhoang2000hn@users.noreply.github.com>
2020-06-18 08:49:58 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -9,7 +9,7 @@ if (isset($_POST['pass'])) {
 	# code...
 	$pass = $_POST['pass'];
 }
-$sql = "SELECT * FROM tblAccount WHERE _user='".$username."' AND _pass'".$pass."'";
+$sql = "SELECT * FROM tblACCOUNT WHERE _user='".$username."' AND _pass'".$pass."'";
 $result =pg_query($dbconn, $sql);
 $row = pg_num_rows($result);
 if($row==1)
